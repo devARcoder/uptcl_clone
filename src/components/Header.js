0e68308a9f00ad01a177af7c2418af8586bef409
Abdur Razzaq <abdurrazaq789@gmail.com>
2025-02-22 00:34:33 +0500

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,15 @@ import React from 'react'
 import bell from './Assets/header_icons/bell.svg'
 import search from './Assets/header_icons/search.svg'
 import group from './Assets/header_icons/group.svg'
+import add from './Assets/header_icons/add.svg'
 import profile from './Assets/images/profile.png'
 const Header = () => {
   return (
     <>
-    <div className="flex justify-between items-center border-b border-gray-200 p-3">
+    <div className="flex justify-between items-center border-b border-gray-200 p-4">
         <div className="">
             <img className='realtive w-8 h-8' src={bell} alt="" />
-            <span className='absolute top-2 left-6 bg-orange-500 text-white rounded-full px-1.5 text-[14px]'>3</span>
+            <span className='absolute top-3 left-7 bg-orange-500 text-white rounded-full px-1.5 text-[14px]'>3</span>
         </div>
         <div className="">
             <h1 className='text-green-500 font-semibold text-2xl'><span className='text-orange-500 font-extrabold text-[34px]'>u</span>ptcl</h1>
@@ -19,13 +20,16 @@ const Header = () => {
         </div>
     </div>
 
-    <div className="flex space-x-4">
+    <div className="flex space-x-6 justify-start items-center p-3">
         <div className="border border-orange-500 p-2 rounded-full bg-orange-200 flex items-center">
             <img className='h-8 w-8 bg-orange-500 rounded-full' src={group} alt="" />
         </div>
         <div className="relative">
             <img className=' rounded-full w-14 h-14' src={profile} alt="" />
             <span className='absolute w-8 h-8 text-center bottom-[-10px] left-8 bg-orange-500 font-bold text-white rounded-full border-2 border-white px-1.5 text-[20px]'>u</span>
+        </div>
+        <div className="">
+            <img className='h-16 w-16' src={add} alt="" />
         </div>
     </div>
     </>
